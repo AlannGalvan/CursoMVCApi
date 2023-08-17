7345b9ada5ec1107904c068997e7696ec9e00d77
Alan <alan_galvan97@hotmail.com>
2023-08-17 12:50:17 -0300

    subida

---
+++ CursoMVCApi/Controllers/AccessController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CursoMVCApi.Models.WS;
+
+namespace CursoMVCApi.Controllers
+{
+    public class AccessController : ApiController
+    {
+        [HttpGet]
+        public Reply HelloWorld()
+        {
+            Reply oR = new Reply();
+            oR.result = 1;
+            oR.message = "Hi World";
+
+            return oR;
+        }
+    }
+}
