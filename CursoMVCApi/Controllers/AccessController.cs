803fce78e566bacea556271b8706e45a8cd831ad
Alan <alan_galvan97@hotmail.com>
2023-08-17 18:17:10 -0300

    Autentificación, Access token

--- CursoMVCApi/Controllers/AccessController.cs
+++ CursoMVCApi/Controllers/AccessController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using CursoMVCApi.Models.WS;
+using CursoMVCApi.Models;
+
 
 namespace CursoMVCApi.Controllers
 {
@@ -16,6 +18,41 @@ namespace CursoMVCApi.Controllers
             Reply oR = new Reply();
             oR.result = 1;
             oR.message = "Hi World";
+
+            return oR;
+        }
+
+        [HttpPost]
+        public Reply Login([FromBody]AccessViewModel model)
+        {
+            Reply oR = new Reply();
+            oR.result = 0;
+            try
+            {
+                using ( CursoMVCApiEntities db =new CursoMVCApiEntities() )
+                {
+                    var lst = db.user.Where(d => d.email == model.email && d.password == model.password && d.idEstatus == 1);
+
+                    if(lst.Count() > 0)
+                    {
+                        oR.result = 1;
+                        oR.data = Guid.NewGuid().ToString();
+
+                        user oUser = lst.First();
+                        oUser.token = (string)oR.data;
+                        db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        oR.message = "Datos incorrectos";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                oR.message = "Ocurrio un error, estamos corrigiendolo";
+            }
 
             return oR;
         }
